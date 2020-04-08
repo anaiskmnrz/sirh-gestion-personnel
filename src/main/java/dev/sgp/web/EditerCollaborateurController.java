48d87bb7d7f43b9e60ffe6d3217daf1668ef5162
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-04-08 19:10:00 +0200

    Gestion des erreurs

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -5,9 +5,8 @@ package dev.sgp.web;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
+import java.time.format.DateTimeParseException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -51,19 +50,36 @@ public class EditerCollaborateurController extends HttpServlet{
 		String dateNaissance = req.getParameter("naissance"); 
 		String numSecu = req.getParameter("numeroSecu");
 		
+		if (nom == null || prenom == null || adresse == null || dateNaissance == null || numSecu == null) {
+			resp.setStatus(400);
+			req.getRequestDispatcher("/WEB-INF/views/collab/editerCollaborateurs.jsp").forward(req, resp);
+		} else {
+			resp.setStatus(200);
+		}
 		
-		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-		  //convert String to LocalDate
-		 
-		  LocalDate date = LocalDate.parse(dateNaissance, formatter);
-		  
-		  
+		LocalDate date = null;
+		try {
+			// convert string to LocalDate
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			date = LocalDate.parse(dateNaissance, formatter);
+			resp.setStatus(200);
+		} catch (DateTimeParseException e) {
+			resp.setStatus(400);
+			req.getRequestDispatcher("/WEB-INF/views/collab/editerCollaborateurs.jsp").forward(req, resp);
+		}
 		
+		if(numSecu.length()!=15) {
+			resp.setStatus(400);
+			req.getRequestDispatcher("/WEB-INF/views/collab/editerCollaborateurs.jsp").forward(req, resp);
+		} else {
+			resp.setStatus(200);
+		}
+		
+		if (resp.getStatus()== 200) {
 		Collaborateur collaborateur = new Collaborateur(nom, prenom, date, adresse, numSecu);
 		collabService.sauvegarderCollaborateur(collaborateur);
 		
-		resp.sendRedirect(req.getContextPath() + "/collaborateurs/lister");
+		resp.sendRedirect(req.getContextPath() + "/collaborateurs/lister");}
 		
 	}
 	
